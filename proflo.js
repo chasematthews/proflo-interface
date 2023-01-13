285aa21da782135f7fd66560a90e84bae6d48676
Chase Matthews <chase.matt@outlook.com>
2023-01-13 11:02:28 +1100

    Added functionality where a table will show up when hovering and clicking over the tream numbers

--- proflo.js
+++ proflo.js
@@ -4,21 +4,59 @@ setTimeout(() => {
 
   identifiers.forEach(identifier => {
     if (identifier.textContent.substring(0,2) == "AS") {
+      var streamDiv = identifier;
       identifier.addEventListener('mouseover', () => {
-        identifier.style.backgroundColor = "red";
-        identifier.style.cursor = "pointer";
-        let position = identifier.getBoundingClientRect();
-        const streamInfo = document.createElement("div");
-        streamInfo.classList.add('stream-info')
-        streamInfo.style.left = `${position.left + position.width}px`
-        streamInfo.style.top = `${position.top + position.height}px`
-        rightContent.appendChild(streamInfo);
+        showInfo(identifier, rightContent);
       });
       identifier.addEventListener('mouseout', () => {
-        identifier.style.background = "none";
-        streamInfo = document.querySelector(".stream-info");
-        streamInfo.remove();
+        hideInfo(streamDiv, rightContent);
+      });
+      identifier.addEventListener('click', () => {
+        identifier.removeEventListener('mouseOut', () => {
+          hideInfo(streamDiv, rightContent);
+        });
+        showInfo(identifier, rightContent);
       });
     };
   });
+
 }, 100);
+
+function showInfo(streamDiv, rightContent) {
+  streamDiv.style.backgroundColor = "red";
+  streamDiv.style.cursor = "pointer";
+  let position = streamDiv.getBoundingClientRect();
+  const streamInfo = document.createElement("div");
+  streamInfo.classList.add('stream-info');
+  streamInfo.style.left = `${position.left + position.width}px`
+  streamInfo.style.top = `${position.top + position.height}px`
+  rightContent.appendChild(streamInfo);
+  buildTable(streamInfo);
+}
+
+function hideInfo(streamDiv, rightContent) {
+  streamDiv.style.background = "none";
+  streamInfo = document.querySelector(".stream-info");
+  streamInfo.remove();
+}
+
+function buildTable(streamInfo) {
+  const stHeading = document.createElement("h2");
+  stHeading.textContent = "Stream Information";
+  streamInfo.appendChild(stHeading);
+
+  const table = document.createElement("table");
+  streamInfo.appendChild(table);
+
+  for (let row = 0; row <= 10; row++) {
+    const rowElement = document.createElement("tr");
+    rowElement.classList.add("table-cell");
+    table.appendChild(rowElement);
+    for (let col = 0; col <= 1; col++) {
+      const cellElement = document.createElement("td");
+      cellElement.classList.add("table-cell");
+      cellElement.textContent = `row: ${row}, column: ${col}`;
+      rowElement.appendChild(cellElement);
+    };
+  };
+};
